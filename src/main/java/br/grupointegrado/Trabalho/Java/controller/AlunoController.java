975b25b77861a4a8443d0e1755f3807a159b0886
Riyuiti Mizuno <riyuitimizuno02@gmail.com>
2024-11-26 22:19:48 -0300

    segundo commit

--- src/main/java/br/grupointegrado/Trabalho/Java/controller/AlunoController.java
+++ src/main/java/br/grupointegrado/Trabalho/Java/controller/AlunoController.java
@@ -1,22 +1,62 @@
 package br.grupointegrado.Trabalho.Java.controller;
 
+import br.grupointegrado.Trabalho.Java.dto.AlunoRequestDTO;
 import br.grupointegrado.Trabalho.Java.model.Aluno;
 import br.grupointegrado.Trabalho.Java.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/alunos")
 public class AlunoController {
 
     @Autowired
     private AlunoRepository repository;
 
 
-    @GetMapping("/alunos")
+    @GetMapping
     public List<Aluno> findAll() {
         return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Aluno findById(@PathVariable Integer id){
+        return this.repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado"));
+    }
+
+    @PostMapping
+    public Aluno save(@RequestBody AlunoRequestDTO dto){
+        Aluno aluno = new Aluno();
+        aluno.setNome(dto.nome());
+        aluno.setEmail(dto.email());
+        aluno.setMatricula(dto.matricula());
+        aluno.setData_nascimento(dto.data_nascimento());
+
+        return this.repository.save(aluno);
+    }
+
+    @PutMapping("/{id}")
+    public Aluno update(@PathVariable Integer id,
+                        @RequestBody AlunoRequestDTO dto){
+        Aluno aluno = this.repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado"));
+
+        aluno.setNome(dto.nome());
+        aluno.setEmail(dto.email());
+        aluno.setMatricula(dto.matricula());
+        aluno.setData_nascimento(dto.data_nascimento());
+
+        return this.repository.save(aluno);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        Aluno aluno = this.repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Aluno não encontrado"));
+        this.repository.delete(aluno);
     }
 }
